2c4f4490b86b6bba1e786edd74b150ecef6d12c9
yassin mohamed <yassin_mohamed310@outlook.com>
2022-04-16 21:49:21 +0200

    Create Header Component

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
-
+import './CSS/index.css'
+import Header from './components/Header/Header'
 function App() {
   return (
-    <div className="App">
-
+    <div className="layout">
+      <Header />
+      <main>
+        Content
+      </main>
+      <footer>
+        Footer
+      </footer>
     </div>
   );
 }
