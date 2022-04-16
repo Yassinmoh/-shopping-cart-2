3dc870626b0b406e542e0562f05c5522b470a98e
yassin mohamed <yassin_mohamed310@outlook.com>
2022-04-16 23:30:03 +0200

    Create File data

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,12 +1,13 @@
 import './CSS/index.css'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import {words} from './words'
 function App() {
   return (
     <div className="layout">
       <Header />
       <main>
-        Content
+        {words.Content}
       </main>
       <Footer/>
 
---
+++ Client/src/words.js
@@ -0,0 +1,5 @@
+export const words={
+    hederTitle:"Shopping Cart",
+    FooterTitle:"© Copyright Yassin Mohamed 2022",
+    Content:"Content"
+}
