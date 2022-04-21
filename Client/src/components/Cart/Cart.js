7a5166200ce488585b71ea87e439fb58c694eb3c
yassin mohamed <yassin_mohamed310@outlook.com>
2022-04-21 02:43:37 +0200

    Create Checkout Form Component

--- Client/src/components/Cart/Cart.js
+++ Client/src/components/Cart/Cart.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Cart.css'
-const Cart = ({ cartItems,removeFromCart }) => {
+import Checkout from '../Checkout/Checkout'
+const Cart = ({ cartItems, removeFromCart }) => {
+
+    const [showForm, setShowForm] = useState(false)
+    const [value,setValue]= useState('')
+
+
+
+
+    const submitOrder=(e)=>{
+        e.preventDefault()
+        const order={
+            name:value.name,
+            email:value.email,
+        }
+        console.log(order);
+    }
+
+    const handelChange=(e)=>{
+        setValue((prevStat)=>({...prevStat,[e.target.name]:e.target.value}))
+    }
+
     return (
         <div className="cart-wrapper">
-            <div className="cart-title">{cartItems.length=== 0 ? 'Cart Empty' : `There is ${cartItems.length} Order`}</div>
+            <div className="cart-title">{cartItems.length === 0 ? 'Cart Empty' : `There is ${cartItems.length} Order`}</div>
             <div className="cart-items">
-                {cartItems.map((item) =>(
+                {cartItems.map((item) => (
                     <div className="cart-item" key={item.id}>
                         <img src={item.imageUrl} alt={item.title} />
                         <div className="cart-info">
@@ -14,10 +35,23 @@ const Cart = ({ cartItems,removeFromCart }) => {
                                 <p>Quantity: {item.qty}</p>
                                 <p>Price: ${item.price}</p>
                             </div>
-                            <button onClick={()=>removeFromCart(item)}>Remove</button>
+                            <button onClick={() => removeFromCart(item)}>Remove</button>
                         </div>
                     </div>))}
             </div>
+            {
+                cartItems != 0 && (
+                    <div className="cart-footer">
+                        <div className="totel">Totel:${cartItems.reduce((acc, p) => {
+                            return acc + Number(p.price) * p.qty
+                        }, 0)}
+                        </div>
+                        <button onClick={() => setShowForm(true)}>Select Product</button>
+                    </div>
+                )
+            }
+            {/* Checkout-Form */}
+            <Checkout submitOrder={submitOrder} handelChange={handelChange} showForm={showForm} setShowForm={setShowForm}/>
         </div>
     )
 }
