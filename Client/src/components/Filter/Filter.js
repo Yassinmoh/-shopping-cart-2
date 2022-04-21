688c09553204a9804af3aaf098736c93af3cdb0f
yassin mohamed <yassin_mohamed310@outlook.com>
2022-04-21 17:00:15 +0200

    Add Animation to Components

--- Client/src/components/Filter/Filter.js
+++ Client/src/components/Filter/Filter.js
@@ -1,36 +1,39 @@
 import React from 'react'
 import './Filter.css'
+import Flip from 'react-reveal/Flip';
 const Filter = ({
-                size,
-                sort,
-                handelFilterBySize,
-                handelFilterBySort,
-                productNumber
-            }) => {
+    size,
+    sort,
+    handelFilterBySize,
+    handelFilterBySort,
+    productNumber
+}) => {
     return (
-        <div className="filter-wrapper">
-            <h2 className="filter-title">Filter</h2>
-            <div className="nom-of-product">number of product {productNumber} </div>
-            <div className="filter-by-size">
-                <span className="span-title">Size</span>
-                <select className="filter-select" onChange={handelFilterBySize} value={size}>
-                    <option value="ALL">ALL</option>
-                    <option value="S">S</option>
-                    <option value="M">M</option>
-                    <option value="L">L</option>
-                    <option value="XL">XL</option>
-                    <option value="XXL">XXL</option>
-                </select>
+        <Flip left cascade>
+            <div className="filter-wrapper">
+                <h2 className="filter-title">Filter</h2>
+                <div className="nom-of-product">number of product {productNumber} </div>
+                <div className="filter-by-size">
+                    <span className="span-title">Size</span>
+                    <select className="filter-select" onChange={handelFilterBySize} value={size}>
+                        <option value="ALL">ALL</option>
+                        <option value="S">S</option>
+                        <option value="M">M</option>
+                        <option value="L">L</option>
+                        <option value="XL">XL</option>
+                        <option value="XXL">XXL</option>
+                    </select>
+                </div>
+                <div className="filter-by-size">
+                    <span className="span-title">Order</span>
+                    <select className="filter-select" onChange={handelFilterBySort} value={sort}>
+                        <option value="lastes">Lastes</option>
+                        <option value="lowest">Lowest</option>
+                        <option value="highest">Highest</option>
+                    </select>
+                </div>
             </div>
-            <div className="filter-by-size">
-                <span className="span-title">Order</span>
-                <select className="filter-select" onChange={handelFilterBySort} value={sort}>
-                    <option value="lastes">Lastes</option>
-                    <option value="lowest">Lowest</option>
-                    <option value="highest">Highest</option>
-                </select>
-            </div>
-        </div>
+        </Flip>
     )
 }
 
