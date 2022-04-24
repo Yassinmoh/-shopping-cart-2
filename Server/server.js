43e2fd480948fadf1ccc8a01b1cb6b8b26d24ea0
yassin mohamed <yassin_mohamed310@outlook.com>
2022-04-24 07:11:01 +0200

    modyfi server file

--- Server/server.js
+++ Server/server.js
@@ -1,21 +1,18 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-const Product =require('./models/Product')
 const ProductRoutes= require('./routes/ProductRoutes')
 require('./DB')
 const app = express()
 // app.use(bodyParser.json())
 app.use(express.json())
 
-// Server
-app.listen(3000, (req, res) => {
-    console.log('server is running on port 3000')
+
+app.listen(4000, (req, res) => {
+    console.log('server is running on port 4000')
 })
 
 app.use('/',ProductRoutes)
 
-// Show All
 
 
 
